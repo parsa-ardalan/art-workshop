f5f92d0949b63f7263d4e03c41408be9a4ae61d1
parsa ardalan <parsardlan2112@gmail.com>
2025-12-25 13:35:51 +0330

    bug fixed. redeploy again

---
+++ pages/contact/index.jsx
@@ -0,0 +1,9 @@
+export default function contact () { 
+
+    return (
+
+        <div> 
+            <h1> contact us page </h1>
+        </div>
+    )
+}
---
+++ pages/courses/index.jsx
@@ -0,0 +1,9 @@
+export default function courses () { 
+
+    return (
+
+        <div> 
+            <h1> courses page </h1>
+        </div>
+    )
+}
